b540aa2037f3dcf4c5ed469dde26037839c7e1c2
David <david.lobry96@gmail.com>
2021-04-21 12:38:56 +0200

    countdown done

--- src/Components/count/Count.tsx
+++ src/Components/count/Count.tsx
@@ -6,26 +6,52 @@ const Count = () => {
     seconds: number;
   }
 
-  const [counter, setCounter] = useState<number>(60);
+  const [seconds, setSeconds] = useState<number>(60);
+  const [minutes, setMinutes] = useState<number>(60);
+  const [hours, setHours] = useState<number>(60 * minutes);
+  const [days, setDays] = useState<number>(24 * hours);
+
+  const launchDate = Date.parse("2021-04-28T08:33:37+0000") / 1000;
+  const difference = launchDate - Date.now() / 1000;
+  const timeDiff = {
+    days: Math.floor(difference / days),
+    hours: Math.floor((difference % days) / hours),
+    minutes: Math.floor((difference % hours) / minutes),
+    seconds: Math.floor(difference % minutes),
+  };
+  console.log("timeDiff:", timeDiff);
+
+  const [secondsLeft, setSecondsLeft] = useState<number>(timeDiff.seconds);
+  const [minutesLeft, setMinutesLeft] = useState<number>(timeDiff.minutes);
+  const [hoursLeft, setHoursLeft] = useState<number>(timeDiff.hours);
+  const [daysLeft, setDaysLeft] = useState<number>(timeDiff.days);
 
   useEffect(() => {
-    //ts-nocheck
-    const timer: any | ReturnType<typeof setTimeout> =
-      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-    return () => clearInterval(timer);
-  }, [counter]);
+    const timer: any | ReturnType<typeof setTimeout> = setInterval(() => {
+      if (secondsLeft > 0) {
+        setSecondsLeft(secondsLeft - 1);
+      }
+      if (secondsLeft === 0) {
+        if (minutesLeft === 0) {
+          clearInterval(timer);
+        } else {
+          setMinutesLeft(minutesLeft - 1);
+          setSecondsLeft(59);
+        }
+      }
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  });
   return (
     <div>
       We're launching soon
-      <div
-        className="numbers"
-        id="countdown"
-        data-time="2021-04-21T08:33:37+0000"
-      >
-        <div className="days">Days</div>
-        <div className="hours">Hours</div>
-        <div className="minutes">Minutes</div>
-        <div className="seconds">{counter}</div>
+      <div className="numbers">
+        <div className="days">{daysLeft}</div>
+        <div className="hours">{hoursLeft}</div>
+        <div className="minutes">{minutesLeft}</div>
+        <div className="seconds">{secondsLeft}</div>
       </div>
     </div>
   );
